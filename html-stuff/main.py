9248f4faa23efc1504a289eb20493ff7472d2d0b
sechilozel <secilozel@hotmail.com>
2025-11-14 13:13:06 +0300

    Created buttons for all filters except for "magicrate"
    Created buttons and coded the basic parts for all filters except for the "magicrate" filter.

--- html-stuff/main.py
+++ html-stuff/main.py
@@ -14,4 +14,20 @@ def sbyname():
 def sbysurname():
     return render_template("surnamebutton.html")
 
+@app.route("/sbyfamily")
+def sbyfamily():
+    return render_template("familybutton.html")
+
+@app.route("/sbynation")
+def sbynation():
+    return render_template("nationbutton.html")
+
+@app.route("/sbyrace")
+def sbyrace():
+    return render_template("racebutton.html")
+
+@app.route("/sbylineage")
+def sbylineage():
+    return render_template("lineagebutton.html")
+
 app.run(debug=True)
