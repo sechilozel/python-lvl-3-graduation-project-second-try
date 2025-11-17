2bedc7fc2fa1c65338ab01a36f6e484baf63be28
sechilozel <secilozel@hotmail.com>
2025-11-17 21:48:01 +0300

    Finished most of the webpage.
    The project is almost done.

--- html-stuff/main.py
+++ html-stuff/main.py
@@ -1,40 +1,168 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
+from logic import DB_Manager
+from config import DATABASE
 
+manager = DB_Manager(DATABASE)
 app = Flask(__name__)
 
 @app.route("/")
 def mainpage():
-    conn = sqlite3.connect('characters.db')
-    cursor = conn.cursor()
-    cursor.execute("SELECT name, surname, family, nation, race, lineage, magicrate FROM characters")
-    characters = cursor.fetchall()
-    conn.close()
-    return render_template('index.html', characters=characters)
+    # conn = sqlite3.connect('characters.db')
+    # cursor = conn.cursor()
+    # cursor.execute("SELECT name, surname, family, nation, race, lineage, magicrate FROM characters")
+    # characters = cursor.fetchall()
+    # conn.close()
+    return render_template('index.html')     # , characters=characters)
 
 
-@app.route("/sbyname")
+@app.route("/sbyname", methods=["GET", "POST"])
 def sbyname():
-    return render_template("namebutton.html")
+    conn = sqlite3.connect('characters.db')
+    cur = conn.cursor()
+    if request.method == 'POST':
+        name_query = request.form['name']
+        cur.execute("""
+            SELECT name, surname, family, nation, race, lineage, magicrate
+            FROM characters
+            WHERE name LIKE ?
+        """, ('%' + name_query + '%',))
+    else:
+        cur.execute("""
+            SELECT name, surname, family, nation, race, lineage, magicrate
+            FROM characters
+        """)
 
-@app.route("/sbysurname")
+    characters = cur.fetchall()
+    conn.close()
+    return render_template('namebutton.html', characters=characters)
+
+
+@app.route("/sbysurname", methods=["GET", "POST"])
 def sbysurname():
-    return render_template("surnamebutton.html")
+    conn = sqlite3.connect('characters.db')
+    cur = conn.cursor()
+    if request.method == 'POST':
+        surname_query = request.form['surname']
+        cur.execute("""
+            SELECT name, surname, family, nation, race, lineage, magicrate
+            FROM characters
+            WHERE surname LIKE ?
+        """, ('%' + surname_query + '%',))
+    else:
+        cur.execute("""
+            SELECT name, surname, family, nation, race, lineage, magicrate
+            FROM characters
+        """)
 
-@app.route("/sbyfamily")
+    characters = cur.fetchall()
+    conn.close()
+    return render_template('surnamebutton.html', characters=characters)
+
+@app.route("/sbyfamily", methods=["GET", "POST"])
 def sbyfamily():
-    return render_template("familybutton.html")
+    conn = sqlite3.connect('characters.db')
+    cur = conn.cursor()
+    if request.method == 'POST':
+        family_query = request.form['family']
+        cur.execute("""
+            SELECT name, surname, family, nation, race, lineage, magicrate
+            FROM characters
+            WHERE family LIKE ?
+        """, ('%' + family_query + '%',))
+    else:
+        cur.execute("""
+            SELECT name, surname, family, nation, race, lineage, magicrate
+            FROM characters
+        """)
 
-@app.route("/sbynation")
+    characters = cur.fetchall()
+    conn.close()
+    return render_template('familybutton.html', characters=characters)
+
+@app.route("/sbynation", methods=["GET", "POST"])
 def sbynation():
-    return render_template("nationbutton.html")
+    conn = sqlite3.connect('characters.db')
+    cur = conn.cursor()
+    if request.method == 'POST':
+        nation_query = request.form['nation']
+        cur.execute("""
+            SELECT name, surname, family, nation, race, lineage, magicrate
+            FROM characters
+            WHERE nation LIKE ?
+        """, ('%' + nation_query + '%',))
+    else:
+        cur.execute("""
+            SELECT name, surname, family, nation, race, lineage, magicrate
+            FROM characters
+        """)
 
-@app.route("/sbyrace")
+    characters = cur.fetchall()
+    conn.close()
+    return render_template('nationbutton.html', characters=characters)
+
+@app.route("/sbyrace", methods=["GET", "POST"])
 def sbyrace():
-    return render_template("racebutton.html")
+    conn = sqlite3.connect('characters.db')
+    cur = conn.cursor()
+    if request.method == 'POST':
+        race_query = request.form['race']
+        cur.execute("""
+            SELECT name, surname, family, nation, race, lineage, magicrate
+            FROM characters
+            WHERE race = ?
+        """, (race_query,))
+    else:
+        cur.execute("""
+            SELECT name, surname, family, nation, race, lineage, magicrate
+            FROM characters
+        """)
 
-@app.route("/sbylineage")
+    characters = cur.fetchall()
+    conn.close()
+    return render_template('racebutton.html', characters=characters)
+
+@app.route("/sbylineage", methods=["GET", "POST"])
 def sbylineage():
-    return render_template("lineagebutton.html")
+    conn = sqlite3.connect('characters.db')
+    cur = conn.cursor()
+    if request.method == 'POST':
+        lineage_query = request.form['lineage']
+        cur.execute("""
+            SELECT name, surname, family, nation, race, lineage, magicrate
+            FROM characters
+            WHERE lineage LIKE ?
+        """, ('%' + lineage_query + '%',))
+    else:
+        cur.execute("""
+            SELECT name, surname, family, nation, race, lineage, magicrate
+            FROM characters
+        """)
+
+    characters = cur.fetchall()
+    conn.close()
+    return render_template('lineagebutton.html', characters=characters)
+
+@app.route("/addcharacter", methods=["GET", "POST"])
+def addcharacter():
+    if request.method == 'POST':
+        name = request.form['name']
+        surname = request.form['surname']
+        family = request.form['family']
+        nation = request.form['nation']
+        race = request.form['race']
+        lineage = request.form['lineage']
+        magicrate = request.form['magicrate']
+        conn = sqlite3.connect('characters.db')
+        cur = conn.cursor()
+        cur.execute("""
+            INSERT INTO characters (name, surname, family, nation, race, lineage, magicrate)
+            VALUES (?, ?, ?, ?, ?, ?, ?)
+        """, (name, surname, family, nation, race, lineage, magicrate))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('mainpage'))
+    
+    return render_template('addbutton.html')
 
 app.run(debug=True)
